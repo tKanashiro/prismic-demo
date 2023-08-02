0b1aaccb5f8e83190f4402ae417c7c9c6f205551
Tammy Kanashiro <tammy.kanashiro@aligent.com.au>
2023-08-02 11:42:14 +0930

    Create test component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import { PrismicRichText, useFirstPrismicDocument } from "@prismicio/react";
+import {
+    PrismicRichText,
+    useAllPrismicDocumentsByType,
+    useFirstPrismicDocument,
+    usePrismicDocumentByUID,
+    usePrismicDocumentsByType,
+    useSinglePrismicDocument,
+} from "@prismicio/react";
 
 function App() {
-    const [document] = useFirstPrismicDocument();
-
+    // const [document] = useFirstPrismicDocument();
+    const [document] = useAllPrismicDocumentsByType("home");
+    console.log("document", document);
     return (
-        <div>{document && <PrismicRichText field={document.data.title} />}</div>
+        <div>
+            {document && <PrismicRichText field={document[0].data.title} />}
+        </div>
     );
 }
 
