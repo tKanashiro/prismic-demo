ebf73fa302ca632905c7e7b3ea3fca27bfdb3bd5
Tammy Kanashiro <tammy.kanashiro@aligent.com.au>
2023-08-03 10:37:38 +0930

    Add page router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages";
+import About from "./pages/about";
+import Navbar from "./components/NavBar";
+
 function App() {
     return (
-        <div>
-            <h1>Test</h1>
-        </div>
+        <>
+            <BrowserRouter>
+                <Navbar />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/about" element={<About />} />
+                </Routes>
+            </BrowserRouter>
+        </>
     );
 }
 
